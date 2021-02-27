68c1ee8ff689ac408ce7abe6fe9a37e537d5d045
Pawel <pjuszcze@gmail.com>
2021-02-27 23:26:45 +0100

    Added: compare word

--- my_regex.py
+++ my_regex.py
@@ -24,3 +24,25 @@ def my_regex(word):
     else:
         return False
 
+def compare_word(word):
+    reg, word = word.split("|")
+
+    if len(reg) == 0 and len(word) == 0:
+        return True
+    elif len(reg) == 0 and len(word) > 1:
+        return True
+    elif len(reg) > 1 and len(word) == 0:
+        return False
+    elif len(reg) <= 1 and len(word) <= 1:
+        return my_regex(reg+"|"+word)
+
+    elif not my_regex(reg[0]+"|"+word[0]):
+        return False
+    else:
+        # print(reg[0], word[0])
+        return compare_word(reg[1:]+"|"+word[1:])
+
+        
+
+
+print(compare_word("a|"))
--- test_my_regex.py
+++ test_my_regex.py
@@ -11,5 +11,21 @@ class test_regex(unittest.TestCase):
         self.assertEqual(my_regex('a|'),  False)
         self.assertEqual(my_regex('a|b'),  False)
 
+    def test_compare_word(self):
+        self.assertEqual(compare_word('a|a'),  True)
+        self.assertEqual(compare_word('.|a'),  True)
+        self.assertEqual(compare_word( '|a'),  True)
+        self.assertEqual(compare_word( '|'),  True)
+        self.assertEqual(compare_word('a|'),  False)
+        self.assertEqual(compare_word('a|b'),  False)
+        self.assertEqual(compare_word('apple|apple'),  True)
+        self.assertEqual(compare_word('.pple|apple'),  True)
+        self.assertEqual(compare_word('appl.|apple'),  True)
+        self.assertEqual(compare_word('.....|apple'),  True)
+        self.assertEqual(compare_word('aplle|apple'),  False)
+        self.assertEqual(compare_word('peach|apple'),  False)
+        self.assertEqual(compare_word('|apple'),  True)
+        self.assertEqual(compare_word('apple|'),  False)
+
 if __name__=="__main__":
     unittest.main()
