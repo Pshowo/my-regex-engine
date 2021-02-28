8de9b55d4c67faf440afebf2b781ca0e62f1de7c
Pawel <pjuszcze@gmail.com>
2021-02-28 21:49:09 +0100

    Added: operators ^

--- test_my_regex.py
+++ test_my_regex.py
@@ -18,12 +18,14 @@ class test_regex(unittest.TestCase):
         self.assertEqual(compare_word( '|'),  True)
         self.assertEqual(compare_word('a|'),  False)
         self.assertEqual(compare_word('a|b'),  False)
+
         self.assertEqual(compare_word('apple|apple'),  True)
         self.assertEqual(compare_word('.pple|apple'),  True)
         self.assertEqual(compare_word('appl.|apple'),  True)
         self.assertEqual(compare_word('.....|apple'),  True)
         self.assertEqual(compare_word('aplle|apple'),  False)
         self.assertEqual(compare_word('peach|apple'),  False)
+
         self.assertEqual(compare_word('|apple'),  True)
         self.assertEqual(compare_word('apple|'),  False)
         self.assertEqual(compare_word('ap|apple'),  True)
@@ -31,7 +33,22 @@ class test_regex(unittest.TestCase):
         self.assertEqual(compare_word('a|apple'),  True)
         self.assertEqual(compare_word('.|apple'),  True)
         self.assertEqual(compare_word('apwle|apple'),  False)
+
+        self.assertEqual(compare_word('^app|apple'),  True)
+        self.assertEqual(compare_word('^a|apple'),  True)
+        self.assertEqual(compare_word('^le|apple'),  False)
+        self.assertEqual(compare_word('^apple|apple pie'),  True)
+
         """
+        self.assertEqual(compare_word('le$|apple'),  True)
+        self.assertEqual(compare_word('.$|apple'),  True)
+        self.assertEqual(compare_word('app$|apple'),  False)
+        self.assertEqual(compare_word('apple$|tasty apple'),  True)
+        
+        self.assertEqual(compare_word('^apple$|apple'),  True)
+        self.assertEqual(compare_word('^apple$|tasty apple'),  False)
+        self.assertEqual(compare_word('^apple$|apple pie'),  False)
+
         """
 
 if __name__=="__main__":
